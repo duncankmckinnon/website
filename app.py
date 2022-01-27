3cf6f44fda49f17195b0c8e7d89bc57dc13bf1d1
Duncan McKinnon <duncanmckinnon96@gmail.com>
2022-01-26 18:01:09 -0800

    adding react app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+from flask_restful import Api
+from api.Handler import Handler
 
+app = Flask(__name__, static_url_path = '')
+api = Api(app)
 
-@app.route("/")
-def home():
-    return render_template('home.html')
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+api.add_resource(Handler, '/flask/home')
